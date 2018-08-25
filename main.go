3adc4d41196ced9e69dd816f8ffcb7c0dd1668fc
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-08-26 03:40:52 +0900

    add assets file

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("[searcher]: server start")
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/dashboard")
+	})
+
+	r.GET("/dashboard", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "board.html", gin.H{})
+	})
+
+	r.Static("/assets", "./assets")
+
+	r.Run(":8888")
+}
