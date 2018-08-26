34a5bdc8746d2cb4dd82da4f154454e1805253a3
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-08-27 00:41:47 +0900

    fixed models/user.go

--- models/user.go
+++ models/user.go
@@ -1,8 +1,27 @@
 package models
 
+import (
+	"net"
+)
+
 type User struct {
 	UserId int `xorm:"user_id not null BIGINT pk autoincr"`
 	UserName string `xorm:"user_name not null"`
 	HwAddr string `xorm:"mac_address not null"`
 	IconPath string `xorm:"icon_path"`
 }
+
+func (u *User) GetUserId() int { return u.UserId }
+
+func (u *User) GetUserName() string { return u.UserName }
+
+func (u *User) GetHwAddr() ( net.HardwareAddr, error) {
+	hwAddr, err := net.ParseMAC(u.HwAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return hwAddr, nil
+}
+
+func (u *User) GetIconPath() string { return u.IconPath }
